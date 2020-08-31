210f2378d1f051ced21445365ccf00f798a668ed
Nikola_Mandic <nikola.mandic@liveweb.io>
2020-08-31 15:20:39 +0200

    First commit

---
+++ src/render.js
@@ -0,0 +1,50 @@
+const videoSelectBtn = document.getElementById('videoSelectBtn');
+videoSelectBtn.onclick = getVideoSources;
+
+const { desktopCapturer, remote } = require('electron');
+const { Menu } = remote;
+
+async function getVideoSources() {
+  const inputSources = await desktopCapturer.getSources({
+    types: ['window', 'screen']
+  });
+
+  const videoOptionsMenu = Menu.buildFromTemplate(
+    inputSources.map(source => {
+      return {
+        label: source.name,
+        click: () => selectSource(source)
+      };
+    })
+  );
+
+
+  videoOptionsMenu.popup();
+}
+let mediaRecorder; // MediaRecorder instance to capture footage
+const recordedChunks = [];
+
+// Change the videoSource window to record
+async function selectSource(source) {
+
+  videoSelectBtn.innerText = source.name;
+
+  const constraints = {
+    audio: false,
+    video: {
+      mandatory: {
+        chromeMediaSource: 'desktop',
+        chromeMediaSourceId: source.id
+      }
+    }
+  };
+
+  // Create a Stream
+  const stream = await navigator.mediaDevices
+    .getUserMedia(constraints);
+
+  // Preview the source in a video element
+  const videoElement = document.getElementById("videoElement");
+  videoElement.srcObject = stream;
+  videoElement.play();
+}
